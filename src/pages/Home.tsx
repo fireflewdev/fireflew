1f25f79c8ad46de9eac023f809e2633eb94f99fa
Gordi Tenev <150630157+TipzyGordi@users.noreply.github.com>
2025-01-15 02:12:47 -0500

    check out

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="App-body-top">
             <Header text="fireflew.top" />
             <span>
-                Welcome! I keep this site updated with the latest things I've been working on.{"\n"}Check out these categories to get started:
+                Welcome! I keep this site updated with the latest things I've been working on.{"\n\n"}Check out these categories to get started:
             </span>
             <div style={{ height: padding * 2 }}></div>
             <FFButton title="Music" onClick={() => router.navigate("/music")} />
